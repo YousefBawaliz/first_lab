49935dbb69256185dcd6d17dc5ccb73d220a6e51
YousefBawaliz <Yousef2k012@gmail.com>
2022-08-27 16:21:33 +0300

    updated

--- main.dart
+++ main.dart
@@ -1,16 +1,12 @@
 void main() {
-  
   Map myInfo = {
-
-    "first name" : "yousef",
+    "first name": "yousef",
     "last name": "bawaliz",
-    "Age" : 24,
-    "Education info" : {
-      "University" : "Al Yarmouk University",
-      "Faculty" : "IT",
-      "year" : 2022,
+    "Age": 24,
+    "Education info": {
+      "University": "Al Yarmouk University",
+      "Faculty": "IT",
+      "year": 2022,
     }
-
-  }
-
+  };
 }
